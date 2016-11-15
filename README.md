d0320b99b5c1fca61e626f591808fac5fa6645c7
DZUBA <zzzdzuba@gmail.com>
2016-11-15 13:03:03 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_task_3
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef_task_3
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.

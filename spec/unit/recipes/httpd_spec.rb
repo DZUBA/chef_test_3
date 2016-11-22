a536fc0d1e2975b221b59014f37206eaa6c1d691
DZUBA <zzzdzuba@gmail.com>
2016-11-22 20:40:17 +0200

    Unit test for apache

--- spec/unit/recipes/httpd_spec.rb
+++ spec/unit/recipes/httpd_spec.rb
@@ -4,17 +4,18 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-require 'spec_helper'
+require 'chefspec'
 
 describe 'chef_task_3::httpd' do
-  context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
+  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+  it 'installs httpd' do
+    stub_command("/usr/sbin/apache2 -t").and_return(true)
+    expect(chef_run).to install_package('httpd')
+  end
+
+  it 'enables httpd' do
+    stub_command("/usr/sbin/apache2 -t").and_return(true)
+    expect(chef_run).to enable_service('httpd')
   end
 end
